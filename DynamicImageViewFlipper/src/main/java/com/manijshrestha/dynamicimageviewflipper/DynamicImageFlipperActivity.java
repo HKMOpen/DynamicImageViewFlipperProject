5f2222cf1d854c0106fe8fdd4454bbfc69fd525d
Manij Shrestha <manijshrestha@gmail.com>
2013-12-16 00:11:34 -0600

    Initial Commit

---
+++ DynamicImageViewFlipper/src/main/java/com/manijshrestha/dynamicimageviewflipper/DynamicImageFlipperActivity.java
@@ -0,0 +1,14 @@
+package com.manijshrestha.dynamicimageviewflipper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DynamicImageFlipperActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamicimageflipper);
+
+    }
+}
